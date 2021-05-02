182a5334d4eca547d189a36d9dbe2ece0c8f1eea
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2021-05-03 00:22:41 +0200

    CS-108: Added initial implementation for the Data and Auth products

---
+++ connect-csharp/Src/Products/Auth.cs
@@ -0,0 +1,35 @@
+﻿using Dapi.Response;
+
+namespace Dapi.Products {
+    public class Auth {
+        private string appSecret { get; }
+
+        public Auth(string appSecret) {
+            this.appSecret = appSecret;
+        }
+
+        internal ExchangeTokenResponse exchangeToken(string accessCode, string connectionID) {
+            // Create the request body of this call
+            var reqBody = new ExchangeTokenRequest(appSecret, accessCode, connectionID);
+
+            // Make the request and get the response
+            var respBody = DapiRequest.Do<ExchangeTokenRequest, ExchangeTokenResponse>(reqBody, reqBody.action);
+
+            // return the data if it's valid, otherwise return an error response
+            return respBody ?? new ExchangeTokenResponse("UNEXPECTED_RESPONSE", "Unexpected response body");
+        }
+
+        private class ExchangeTokenRequest {
+            internal string action => "/auth/ExchangeToken";
+            public string appSecret { get; }
+            public string accessCode { get; }
+            public string connectionID { get; }
+
+            public ExchangeTokenRequest(string appSecret, string accessCode, string connectionID) {
+                this.appSecret = appSecret;
+                this.accessCode = accessCode;
+                this.connectionID = connectionID;
+            }
+        }
+    }
+}
---
+++ connect-csharp/Src/Products/DapiRequest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Dapi.Types;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Dapi.Products {
+    public class DapiRequest {
+        // public static readonly string Dapi_URL = "https://api.dapi.co";
+        public static readonly string Dapi_URL = "http://localhost:8090";
+        public static readonly string DD_URL = "https://dd.dapi.co";
+
+        private static readonly IRestClient httpClient = new RestClient()
+            //.UseSerializer(() => new JsonSerializer {DateFormat = "yyyy-mm-dd"})
+            ;
+
+        internal static ResT Do<ReqT, ResT>(ReqT reqBody, string url) {
+            return Do<ReqT, ResT>(reqBody, url, new List<KeyValuePair<string, string>>());
+        }
+
+        internal static ResT Do<ReqT, ResT>(ReqT reqBody, string action, ICollection<KeyValuePair<string, string>> headers) {
+            // create, execute the request, and get the response
+            var url = Dapi_URL + "/v2" + action;
+            var resp = doReq(reqBody, url, headers);
+
+            // convert the got response body to the required type
+            var data = JsonConvert.DeserializeObject<ResT>(resp.Content);
+
+            // return the data if it's valid, otherwise return an error response
+            return data;
+        }
+
+        private static IRestResponse doReq<ReqT>(ReqT reqBody, string url, ICollection<KeyValuePair<string, string>> headers) {
+            // create the request with the passed body and headers
+            var req = new RestRequest(url)
+                .AddJsonBody(reqBody)
+                .AddHeaders(headers);
+
+            // execute the request and return the raw response type
+            return httpClient.Execute(req, Method.POST);
+        }
+
+        public class BaseRequest {
+            public string appSecret { get; }
+            public string userSecret { get; }
+            public string operationID { get; }
+            public UserInput[] userInputs { get; }
+
+            public BaseRequest(string appSecret, string userSecret, string operationID, UserInput[] userInputs) {
+                this.appSecret = appSecret;
+                this.userSecret = userSecret;
+                this.operationID = operationID;
+                this.userInputs = userInputs;
+            }
+        }
+    }
+}
