8a87e84466bc4e8c5083912e20a2a81a4f92e858
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2021-05-03 13:27:07 +0200

    CS-108: Added the response types for the Payment and Metadata products

---
+++ dapi-csharp/Src/Response/TransferAutoflowResponse.cs
@@ -0,0 +1,25 @@
+using Dapi.Types;
+using Newtonsoft.Json;
+
+namespace Dapi.Response {
+    public class TransferAutoflowResponse : BaseResponse {
+        public string reference { get; }
+
+        /// <summary>
+        /// This is used only to automate the deserialization of the got response.
+        /// This is a private constructor to this lib.
+        /// </summary>
+        [JsonConstructor]
+        internal TransferAutoflowResponse(string reference, APIStatus status, bool success, string operationID, UserInput[] userInputs, string type, string msg) :
+            base(status, success, operationID, userInputs, type, msg) {
+            this.reference = reference;
+        }
+
+        /// <summary>
+        /// This is used to construct an error response from the reading of the got response.
+        /// This is a private constructor to this lib.
+        /// </summary>
+        internal TransferAutoflowResponse(string errType, string errMsg) : base(errType, errMsg) {
+        }
+    }
+}
