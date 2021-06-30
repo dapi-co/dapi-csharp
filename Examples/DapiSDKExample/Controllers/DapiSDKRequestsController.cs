0967abe5f7b136de02b2ab00d6db80a19f4beee7
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2021-06-30 21:11:40 +0200

    CS-327: Added an SDK example

---
+++ Examples/DapiSDKExample/Controllers/DapiSDKRequestsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace DapiSDKExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DapiSDKRequestsController : ControllerBase
+    {
+        // if you plan to use this controller for both views and web APIs,
+        // then make the base clase `Controller` instead of `ControllerBase`.
+
+        private readonly ILogger<DapiSDKRequestsController> _logger;
+
+        public DapiSDKRequestsController(ILogger<DapiSDKRequestsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<string> Create([FromBody] JsonElement reqBody)
+        {
+            // Read the headers of the request, and prepare them to be passed to Dapi
+            Dictionary<string, string> requestHeaders = new Dictionary<string, string>();
+            foreach (var header in Request.Headers)
+            {
+                requestHeaders.Add(header.Key, header.Value);
+            }
+
+            // Call Dapi to handle the request, using the created DapiApp instance
+            var resp = Startup.dapiApp.handleSDKRequest(reqBody.GetRawText(), requestHeaders);
+
+            // Check the state of the response got from Dapi
+            if (resp.IsSuccessful)
+            {
+                // Handle the response
+                return resp.Content;
+            }
+            else
+            {
+                // Handle the error
+                return resp.ErrorMessage;
+            }
+        }
+    }
+}
---
+++ Examples/DapiSDKExample/README.md
@@ -0,0 +1,19 @@
+﻿# DapiSDKExample
+
+This example shows how [Dapi's C# library](https://github.com/dapi-co/dapi-csharp) could be used as a server endpoint to handle Dapi requests forwarded to it.
+
+>The example shows a very basic server.   
+
+
+**How to run it**:
+- Open the example in VisualStudio (or any equivalent IDE).
+- Edit the `Startup.cs` file to replace the `YOUR_APP_SECRET` constant passed to the `DapiApp` constructor, at the very start of the file, with you Dapi application's `AppSecret`.
+- Run the example from VisualStudio.
+- The server is now listening on: http://localhost:43206 (by default).
+  - Note: you can change the port of the server from the `Properties/lauchSettings.json` file, through the `applicationUrl` field(s).
+- The endpoint is now exposed on: http://localhost:43206/DapiSDKRequests (by default).
+  - Note: you can change the endpoint name from the `Properties/lauchSettings.json` file, through the `launchUrl` field(s).
+- Use Postman (or any equivalent app), to POST valid Dapi requests to the endpoint `URL`.
+  - All requests' bodies should be in `JSON` format.
+- All requests should pass the `Authorization` header, with its value set to the `accessToken` as a `Bearer` token.
+  - The `accessToken` is retrieved from the [ExchangeToken](https://dapi-api.readme.io/docs/exchange-token) process.
