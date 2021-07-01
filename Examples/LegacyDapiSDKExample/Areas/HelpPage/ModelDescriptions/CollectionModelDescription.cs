d04f6f71e7a668c57bc547be956c4ad258979bfc
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2021-07-01 17:05:28 +0200

    CS-339: Added a new example that works with older .NET versions

---
+++ Examples/LegacyDapiSDKExample/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LegacyDapiSDKExample.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
